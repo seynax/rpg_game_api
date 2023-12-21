e9fd555dd4d86dcaf25fb6a079047f0d64f55cf1
Seynax <seyronax@gmail.com>
2023-12-21 11:31:37 +0100

    views fix

--- game/game_flask.py
+++ game/game_flask.py
@@ -20,13 +20,13 @@ def get_players():
 def get_player(player_id):
    return flask.render_template('index.html', players_stats_list=get_player(player_id))
 
-@app.route('/add/player', methods=['GET', 'POST'])
+@app.route('/player/add', methods=['GET', 'POST'])
 def add_player():
-   values = get_form_values(["name", "zone_active", "attack", "attack_speed", "defense", "life", "regeneration_speed", "level"])
-   if values == None:
-      return flask.render_template('add.html')
+   player = get_form_values(["name", "zone_active", "attack", "attack_speed", "defense", "life", "regeneration_speed", "level"])
+   if player == None:
+      return flask.render_template('player_add.html')
 
-
+   insert_player(player)
 
    return flask.redirect('/')
 
@@ -54,16 +54,18 @@ def api_add_players():
    player = get_json_array_values(["name", "zone_active", "attack", "attack_speed", "defense", "life", "regeneration_speed", "level"])
    return json_success_message("Players added") if insert_players(player) else json_failed_message("Players add")
 
-@app.route('/delete/<player_id>')
-def api_delete_player(player_id):
-   commit_request("DELETE FROM players WHERE player_id = " + str(player_id))
+@app.route('/api/delete/player/')
+def api_delete_player():
+   return json_failed_message("Player deletion not supported")
 
-   return flask.redirect('/')
+@app.route('/api/delete/players/')
+def api_delete_players():
+   return json_failed_message("Players deletion not supported")
 
 ## AREAS
 @app.route('/areas')
-def Area():
-   return flask.render_template('Area.html', areas_list=get_areas_and_path_list())
+def areas():
+   return flask.render_template('areas.html', areas_list=get_areas_and_path_list())
 
 @app.route('/api/areas', methods=['GET'])
 def get_areas():
@@ -111,10 +113,13 @@ def get_json_array_values(values_names):
 
 ## DATABASE UTILS METHODS
 
-def commit_request(request, values):
+def commit_request(request, values=None):
    connection = sqlite3.connect('../resources/SQLITE/rpg_game.db')
    cursor = connection.cursor()
-   cursor.execute(request, values)
+   if values == None:
+      cursor.execute(request)
+   else:
+      cursor.execute(request, values)
    selecteds = cursor.fetchall()
    connection.commit()
    connection.close()
@@ -124,7 +129,7 @@ def commit_request(request, values):
 def select_request(request):
    connection = sqlite3.connect('../resources/SQLITE/rpg_game.db')
    cursor = connection.cursor()
-   cursor.execute("select" + request)
+   cursor.execute("select " + request)
    selecteds = cursor.fetchall()
    connection.close()
 
@@ -149,8 +154,8 @@ def insert_player(player):
    if player == None:
       return False
 
-   if not "zone_active" in player:
-      player["zone_active"]        = 1 ## Make zone active text box in form
+   ##if 'zone_active' not in player:
+   player["zone_active"]        = 1 ## Make zone active text box in form
    commit_request("""
                      INSERT INTO players( player_name, player_zone_active, player_attack, player_attack_speed, player_defense, player_life,  player_regeneration_speed,  player_level)
                                  VALUES (:name,       :zone_active,       :attack,       :attack_speed,       :defense,              :life, :regeneration_speed,        :level)
@@ -185,7 +190,7 @@ def get_areas_and_path_list():
 
    for area in areas:
       cursor.execute("SELECT areas_paths.area_source_id, areas_paths.area_destination_id FROM areas_paths WHERE areas_paths.area_source_id = " + str(area[0]))
-      areas_source_paths = cursor.fetchget_playerall()
+      areas_source_paths = cursor.fetchall()
       cursor.execute("SELECT areas_paths.area_destination_id, areas_paths.area_source_id FROM areas_paths WHERE areas_paths.area_destination_id = " + str(area[0]))
       areas_destination_paths = cursor.fetchall()
 
