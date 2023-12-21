cda1b94a896ccbfa3c7a7d5fd6a0aeb5f2aadaf4
Seynax <seyronax@gmail.com>
2023-12-21 10:11:29 +0100

    Areas API function AND api request

--- game/game_flask.py
+++ game/game_flask.py
@@ -31,6 +31,7 @@ def add():
    if flask.request.method == 'POST':
       parameters = {}
       parameters["name"]               = flask.request.values.get('name')
+      parameters["zone_active"]        = 1
       parameters["attack"]             = flask.request.values.get('attack')
       parameters["attack_speed"]       = flask.request.values.get('attack_speed')
       parameters["defense"]            = flask.request.values.get('defense')
@@ -41,7 +42,7 @@ def add():
       connection = sqlite3.connect('../resources/SQLITE/rpg_game.db')
 
       cursor = connection.cursor()
-      cursor.execute('INSERT INTO players (player_name, player_attack, player_attack_speed, player_defense, player_life, player_regeneration_speed, player_level) VALUES(:name, :attack, :attack_speed, :defense, :life, :regeneration_speed, :level)',
+      cursor.execute('INSERT INTO players (player_name, player_zone_active, player_attack, player_attack_speed, player_defense, player_life, player_regeneration_speed, player_level) VALUES(:name, :zone_active, :attack, :attack_speed, :defense, :life, :regeneration_speed, :level)',
                      parameters)
       connection.commit()
       connection.close()
@@ -65,29 +66,33 @@ def delete(player_id):
 def Area():
    connection = sqlite3.connect('../resources/SQLITE/rpg_game.db')
    cursor = connection.cursor()
-   cursor.execute("""
-                  SELECT areas.area_id, areas.area_name, areas.area_max_entity_count, asp.area_source_id, asp.area_destination_id, bsp.area_source_id, bsp.area_destination_id FROM areas
-                     left outer JOIN areas_paths asp ON areas.area_id = asp.area_source_id
-                     left outer JOIN areas_paths bsp ON areas.area_id = bsp.area_destination_id 
-                  GROUP BY(asp.area_source_id, asp.area_destination_id)
-                  """)
+   cursor.execute('SELECT * FROM areas')
    areas = cursor.fetchall()
 
-   connection.close()
    areas_list = []
 
    for area in areas:
-     areas_list.append({
-        "area_id":                   area[0],
-        "area_name":                 area[1],
-        "area_max_entity_count":     area[2],
-        "area_source_id_0":          area[3],
-        "area_destination_id_0":     area[4],
-        "area_source_id_1":          area[5],
-        "area_destination_id_1":     area[6],
-     })
+      cursor.execute("SELECT areas_paths.area_source_id, areas_paths.area_destination_id FROM areas_paths WHERE areas_paths.area_source_id = " + str(area[0]))
+      areas_source_paths = cursor.fetchall()
+      cursor.execute("SELECT areas_paths.area_destination_id, areas_paths.area_source_id FROM areas_paths WHERE areas_paths.area_destination_id = " + str(area[0]))
+      areas_destination_paths = cursor.fetchall()
 
-   return flask.render_template('Area.html')
+      paths = []
+      for source_path in areas_source_paths:
+         paths.append((source_path[0], source_path[1]))
+      for destination_path in areas_destination_paths:
+         paths.append((destination_path[0], destination_path[1]))
+
+      areas_list.append({
+         "area_id":                   area[0],
+         "area_name":                 area[1],
+         "area_max_entity_count":     area[2],
+         "area_paths":                paths
+      })
+
+   connection.close()
+
+   return flask.render_template('Area.html', areas_list=areas_list)
 
 @app.route('/api/players', methods=['GET'])
 def get_players():
@@ -97,10 +102,10 @@ def get_players():
    players = cursor.fetchall()
    connection.close()
 
-   list_players = []
+   players_list = []
 
    for player in players:
-     list_players.append({
+     players_list.append({
         "player_id":                   player[0],
         "player_name":                 player[1],
         "player_attack":               player[2],
@@ -111,7 +116,39 @@ def get_players():
         "player_level":                player[7]
      })
 
-   return flask.jsonify(list_players)
+   return flask.jsonify(players_list)
+
+@app.route('/api/areas', methods=['GET'])
+def get_areas():
+   connection = sqlite3.connect('../resources/SQLITE/rpg_game.db')
+   cursor = connection.cursor()
+   cursor.execute('SELECT * FROM areas')
+   areas = cursor.fetchall()
+
+   areas_list = []
+
+   for area in areas:
+      cursor.execute("SELECT areas_paths.area_source_id, areas_paths.area_destination_id FROM areas_paths WHERE areas_paths.area_source_id = " + str(area[0]))
+      areas_source_paths = cursor.fetchall()
+      cursor.execute("SELECT areas_paths.area_destination_id, areas_paths.area_source_id FROM areas_paths WHERE areas_paths.area_destination_id = " + str(area[0]))
+      areas_destination_paths = cursor.fetchall()
+
+      paths = []
+      for source_path in areas_source_paths:
+         paths.append((source_path[0], source_path[1]))
+      for destination_path in areas_destination_paths:
+         paths.append((destination_path[0], destination_path[1]))
+
+      areas_list.append({
+         "area_id":                   area[0],
+         "area_name":                 area[1],
+         "area_max_entity_count":     area[2],
+         "area_paths":                paths
+      })
+
+   connection.close()
+
+   return flask.jsonify(areas_list)
 
 '''
 @app.route('/api/dogs', methods=['POST'])
